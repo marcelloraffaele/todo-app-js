2e854108c8a6d8ff85420336d0dd0c72fa9d0d46
Raffaele Marcello <rmarcello@microsoft.com>
2025-04-14 17:21:59 +0200

    fix: mask Application Insights connection string in config

--- todo-frontend/public/config.js
+++ todo-frontend/public/config.js
@@ -1,5 +1,5 @@
 window._env_ = {
     BACKEND_URL: "http://localhost:3000",
-    APPLICATIONINSIGHTS_CONNECTION_STRING: "InstrumentationKey=5775a8fe-2c9c-424c-9fb0-9163bafd9ba1;IngestionEndpoint=https://italynorth-0.in.applicationinsights.azure.com/;LiveEndpoint=https://italynorth.livediagnostics.monitor.azure.com/;ApplicationId=a08dfd8b-e02c-459a-86a6-d23d56c17176",
-    APP_NAME: "todo-app-frontend",
+    APPLICATIONINSIGHTS_CONNECTION_STRING: "InstrumentationKey...",
+    APP_NAME: "todo-app-frontend"
 };
